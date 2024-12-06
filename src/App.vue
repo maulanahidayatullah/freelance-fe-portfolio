1ed4c1f800c32f4d260c3fb3da463dbdc943a33f
LaNa-17 <atomx361@gmail.com>
2024-12-06 22:59:05 +0700

    commit lgi

---
+++ src/App.vue
@@ -0,0 +1,86 @@
+<template>
+  <transition name="fade" tag="div" class="wrapper" mode="out-in">
+    <div class="wrapper" v-if="isLoaded" id="app">
+      <LandingPage />
+      <!-- <Description :user="userData.user" :content="userData.description" :links="userData.links" /> -->
+      <!-- <Experience :content="userData.experiences" /> -->
+      <Skills :skills="userData.bucket.objects[4]" />
+      <Projects :projects="userData.bucket.objects[5].metafields[0].value" />
+      <Footer :user="userData" :links="userData.links" />
+    </div>
+  </transition>
+</template>
+
+<script>
+
+import LandingPage from "./components/LandingPage.vue";
+// import Description from "./components/Description.vue";
+// import Experience from "./components/Experience.vue";
+import Skills from "./components/Skills.vue";
+import Projects from "./components/Projects.vue";
+import Footer from "./components/Footer.vue";
+
+import userData from "../data/data.json";
+
+export default {
+  name: "App",
+
+  components: {
+    LandingPage,
+    // Description,
+    // Experience,
+    Skills,
+    Projects,
+    Footer,
+  },
+  data: () => ({
+    isLoaded: true,
+
+  }),
+  computed: {
+    userData() {
+      // console.log("Yeyy ==>. ", userData.bucket.objects);
+      return userData
+    }
+  },
+  methods: {
+  },
+  created() {
+    // document.body.classList.add("loading");
+
+    // Promise.all([this.fetchPosts(), this.fetchSkills(), this.fetchProjects(), this.fetchUser()]).then(([posts, skills, projects, user_data]) => {
+    //   user_data = this.extractFirstObject(user_data);
+    //   this.posts = posts.objects;
+    //   console.log("user skills ==>> ", projects.objects)
+    //   this.skills = skills.objects;
+    //   this.projects = projects.objects;
+    //   this.user = {
+    //     name: user_data.metadata.name,
+    //     status: user_data.metadata.status,
+    //     email: user_data.metadata.email,
+    //     phone: user_data.metadata.phone,
+    //     city: user_data.metadata.city,
+    //     lang: user_data.metadata.lang,
+    //     photo: user_data.metadata.photo,
+    //   }
+    //   this.isLoaded = true;
+    //   this.$nextTick(() => document.body.classList.remove("loading"));
+    // });
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import "./styles/constants.scss";
+
+#app {
+  font-family: Montserrat-Regular, serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+.wrapper {
+  height: 100%;
+}
+</style>
---
+++ src/components/SocialBar.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="social-wrap">
+    <ul class="social-bar">
+      <li>
+        <a :href="links.facebook" target="_blank">
+          <font-awesome-icon class="icon" :icon="['fab', 'facebook-square']" />
+        </a>
+      </li>
+      <li>
+        <a :href="links.instagram" target="_blank">
+          <font-awesome-icon class="icon" :icon="['fab', 'instagram-square']" />
+        </a>
+      </li>
+      <li>
+        <a :href="links.linkedin" target="_blank">
+          <font-awesome-icon class="icon" :icon="['fab', 'linkedin']" />
+        </a>
+      </li>
+      <li>
+        <a :href="links.github" target="_blank">
+          <font-awesome-icon class="icon" :icon="['fab', 'github-square']" />
+        </a>
+      </li>
+      <li>
+        <a :href="links.stackoverflow" target="_blank">
+          <svg style="margin-bottom: 1.4rem;" xmlns="http://www.w3.org/2000/svg" width="30" height="30"
+            viewBox="0 0 384 512">
+            <path
+              d="M290.7 311L95 269.7 86.8 309l195.7 41zm51-87L188.2 95.7l-25.5 30.8 153.5 128.3zm-31.2 39.7L129.2 179l-16.7 36.5L293.7 300zM262 32l-32 24 119.3 160.3 32-24zm20.5 328h-200v39.7h200zm39.7 80H42.7V320h-40v160h359.5V320h-40z" />
+          </svg>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SocialBar',
+  props: ['links'],
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/styles/constants.scss';
+
+.social-wrap {
+  li {
+    display: inline-block;
+    margin-right: 10px;
+  }
+
+  .icon {
+    font-size: 3rem;
+  }
+}
+</style>
